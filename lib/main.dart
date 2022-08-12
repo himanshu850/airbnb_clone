f1f405ca35d500a5a2008f14f6bb4afa7a56b234
himanshu850 <78013269+himanshu850@users.noreply.github.com>
2022-08-12 20:00:34 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      const MyApp(), // Wrap your app
+    );
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Airbnb Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
+      home: const HomePage(),
+    );
+  }
+}
